02f4314c7c8a568a5e4f00af4aed62b0d68ede24
Nicolas Guerrero <saynaysurf@gmail.com>
2020-06-28 15:57:16 -0700

    Created AddString/actions.js functions.

--- app/containers/AddString/actions.js
+++ app/containers/AddString/actions.js
@@ -4,10 +4,36 @@
  *
  */
 
-import { DEFAULT_ACTION } from './constants';
+import {
+  CHANGE_STRING_INPUT,
+  ADD_STRING,
+  ADD_STRING_SUCCESS,
+  ADD_STRING_ERROR,
+} from './constants';
 
-export function defaultAction() {
+export function changeStringInput(string) {
   return {
-    type: DEFAULT_ACTION,
+    type: CHANGE_STRING_INPUT,
+    string,
+  };
+}
+
+export function addString() {
+  return {
+    type: ADD_STRING,
+  };
+}
+
+export function stringAdded(string) {
+  return {
+    type: ADD_STRING_SUCCESS,
+    string,
+  };
+}
+
+export function stringAddingError(error) {
+  return {
+    type: ADD_STRING_ERROR,
+    error,
   };
 }
